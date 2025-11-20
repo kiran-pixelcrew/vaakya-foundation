b19b8a7e8a97da09d073e49a593edc89dbe9a928
KiranBusari <117756907+KIRAN-BUSARI@users.noreply.github.com>
2025-11-20 23:47:32 +0530

    fix: add overflow-x-hidden to prevent horizontal scrolling in layout and VisionMission components

--- src/components/pages/VisionMission.tsx
+++ src/components/pages/VisionMission.tsx
@@ -4,7 +4,7 @@ import { LucideEye, Target } from "lucide-react";
 
 const VisionMission = () => {
   return (
-    <div className="container mx-auto min-h-[60vh] items-center justify-center relative flex px-4 sm:px-0 py-18 sm:py-0">
+    <div className="container overflow-x-hidden mx-auto min-h-[60vh] items-center justify-center relative flex px-4 sm:px-0 py-18 sm:py-0">
       <div className="flex justify-evenly items-center w-full h-auto flex-col md:flex-row gap-8 md:gap-0">
         <motion.div
           className="bg-[#EDF8FF] h-auto p-12 md:p-12 static md:absolute md:left-42 rounded-lg w-full md:w-5/12 lg:w-4/12 flex flex-col gap-4"
